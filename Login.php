96a23414da1b71fcbd5ed9546eda90d0178d6074
jeanpauldione <dionepauljean@gmail.com>
2019-10-01 19:38:38 +0000

    ajout du pour notre projet de gestion de RV

---
+++ Login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <link rel="stylesheet" href="CSS/bootstrap.min.css">
+        <link rel="stylesheet" href="CSS/style.css">
+    </head>
+    <body> 
+         <center>
+            <div class="col-md-6 col-xs-12">
+                <div class="panel panel-info">
+                    <div class="panel-heading">BIENVENUE SUR LA PLATEFORME DE GESTION DES APPRENANTS</div>
+                     <div class="panel-body">
+                <form method="POST"  action="Autentifier.php">
+                    <p>Merci de vous Authentifier</p>
+                    <input type="text" name="username" placeholder="login" required> <br>
+                    <input type="password" name="password" placeholder="password" required> <br>  
+                    <button type="submit">Se Connecter</button>
+
+                </form>
+                </div>
+
+            </div>
+ 
+            </div>
+            </center>
+    </body>
+</html>
